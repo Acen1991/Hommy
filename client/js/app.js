9c248796123ad5234e1c9cee9eeed7acb010f94e
Acen1991 <mohamedhassen-mami@hotmail.fr>
2015-06-09 11:32:26 +0200

    removing wow-tada effect

--- client/js/app.js
+++ client/js/app.js
@@ -7,20 +7,25 @@ $(window).scroll(function() {
     }
 });
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
+
+$(function(){
+    new WOW().init();
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
+
         event.preventDefault();
     });
-});
 
+    if(window.matchMedia("(min-width: 900px)").matches){
+        $(".call-to-action h2").addClass("tada");
+    } else {
 
-$(function(){
-    new WOW().init();
+    }
 });
 
 
